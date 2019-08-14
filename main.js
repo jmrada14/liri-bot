e2e40dda6813a69cfb24aeeb1d7f06bef2658131
jmrada14 <jmrada14@tutanota.com>
2019-08-13 23:11:44 -0400

    some progress

---
+++ main.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+let keys = require("./keys");
+let fs = require("fs");
+let request = require("request");
+let twitter = require("twitter");
+let spotify = require("node-spotify-api")
+
