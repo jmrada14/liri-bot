d8995289d695cf651483865bdd7c13f91e1db8c7
jmrada14 <jmrada14@tutanota.com>
2019-08-13 23:16:57 -0400

    npm installs done

--- main.js
+++ main.js
@@ -5,4 +5,5 @@ let fs = require("fs");
 let request = require("request");
 let twitter = require("twitter");
 let spotify = require("node-spotify-api")
-
+let filename = './log.txt';
+var inquirer = require('inquirer');
